3c3b36f868d12722bc585861cccfee938f96a828
a.namici <a.namici@OptiPlex-360-Andrea>
2016-04-29 14:22:57 +0200

    index

--- js/index.js
+++ js/index.js
@@ -33,19 +33,155 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function () {
-        alert('eja!');
-        cdb = window.sqlitePlugin.openDatabase({name: "app.db", createFromLocation: 1}, function (err) {
+        cdb = window.sqlitePlugin.openDatabase({name: "appd.db", createFromLocation: 1}, function (err) {
             console.log('Open database OK: ' + JSON.stringify(err));
         }, function (err) {
             console.log('Open database ERROR: ' + JSON.stringify(err));
         });
 
-        cdb.executeSql("SELECT campo AS stringlength from prova", [], function (res) {
-            alert('got stringlength: ' + JSON.stringify(res.rows));
-        }, function (error) {
-            alert('SELECT error: ' + error.message);
-        });
+        bindEvents();
+        
+        
     }
 };
 
 app.initialize();
+
+
+function WriteResult(result)
+{
+    $('.content').prepend('<p>' + result + '</p>');
+}
+
+function Randm(min, max)
+{
+     var m = new MersenneTwister(); 
+    return Math.floor(m.random() * (max - min + 1) + min);
+}
+
+ function roll(dice)
+    {
+        var dice = dice.replace(/- */,'+ -');
+        var dice = dice.replace(/D/,'d');
+        var re = / *\+ */;
+        var items = dice.split(re);
+        var res = new Array();
+        var type = new Array();
+        var m = new MersenneTwister();
+        for ( var i=0; i<items.length; i++) {
+            var match = items[i].match(/^[ \t]*(-)?(\d+)?(?:(d)(\d+))?[ \t]*$/);
+            if (match) {
+                var sign = match[1]?-1:1;
+                var num = parseInt(match[2] || "1");
+                var max = parseInt(match[4] || "0");
+                if (match[3]) {
+                    for ( j=1; j<=num; j++) {
+                        res[res.length] = sign * Math.ceil(max*m.random());
+                        type[type.length] = max;
+                    }
+                }
+                else {
+                    res[res.length] = sign * num;
+                    type[type.length] = 0;
+                }
+            } 
+            else return null;
+        }
+        if (res.length == 0) return null;
+        return {"res":res, "type":type};
+    }
+    
+    function dresultStr(data)
+    {
+        var total = 0;
+        var str = "";
+        for (var i=0; i<data.res.length; i++) {
+            total = total + data.res[i];
+            if (data.res.length>1) {
+                if (i) str = str + ((data.res[i])>=0?"+":"-");
+                str = str + Math.abs(data.res[i]);
+                if (data.type[i]) str = str + "<sub>[d"+data.type[i]+"]</sub>";
+            }
+        }
+        str = "<strong>" + total + "</strong>" + (str?"&nbsp;=&nbsp;" + str:'');
+        return str;
+    }
+
+function bindEvents()
+{
+    $.event.special.swipe.scrollSupressionThreshold = 10; // More than this horizontal displacement, and we will suppress scrolling.
+        $.event.special.swipe.horizontalDistanceThreshold = 30; // Swipe horizontal displacement must be more than this.
+        $.event.special.swipe.durationThreshold = 500;  // More time than this, and it isn't a swipe.
+        $.event.special.swipe.verticalDistanceThreshold = 75; // Swipe vertical displacement must be less than this.
+        $('div[data-role="page"]').on("swiperight", function (e)
+        {
+            if ($(e.target).is('input'))
+                return;
+            var elems = $('[role="tab"].ui-state-active').prevAll();
+            if (elems.length)
+            {
+                elems.eq(0).find('a').click();
+            }
+        }).on("swipeleft", function (e)
+        {
+            if ($(e.target).is('input'))
+                return;
+            var elems = $('[role="tab"].ui-state-active').nextAll();
+            if (elems.length)
+            {
+                elems.eq(0).find('a').click();
+            }
+        });
+
+        $('.odds').bind("click", function ()
+        {
+            if ($(this).val() >= Randm(0,100))
+            {
+                switch(Randm(1,3))
+                {
+                    case 1:
+                        WriteResult('Yes!');
+                        break;
+                    case 2:
+                        WriteResult('Sure!');
+                        break;
+                    case 3:
+                        WriteResult('Yes, but...');
+                        break;                        
+                }                
+            }
+            else
+            {
+                switch(Randm(1,3))
+                {
+                    case 1:
+                        WriteResult('No!');
+                        break;
+                    case 2:
+                        WriteResult('Forget It!');
+                        break;
+                    case 3:
+                        WriteResult('No, but...');
+                        break;                        
+                }                
+            }
+            
+        });
+        
+        $('#clear').bind("click",function(){if(confirm("Sure?"))$('.content').html('')});
+        
+        $('#diceroller').bind("click",function()
+        {
+           WriteResult(dresultStr(roll($('#dicerollerval').val()))); 
+        });
+        
+        $('.getrand').bind('click',function()
+        {
+            var table = $(this).attr('id');
+            cdb.executeSql("SELECT "+table+" as field from "+table+" WHERE _ROWID_ >= (abs(random()) % (SELECT max(_ROWID_) FROM "+table+")) LIMIT 1;", [], function (res) {            
+             WriteResult(res.rows.item(0).field);
+        }, function (error) {
+            alert('SELECT error: ' + error.message);
+        });
+        });
+}
